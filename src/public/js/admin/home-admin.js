8cbff2647e6f39278659f194785b33fb23a28426
minhieukt <minhieukt@gmail.com>
2022-11-30 09:48:57 +0700

    done view info merchant

--- src/public/js/admin/home-admin.js
+++ src/public/js/admin/home-admin.js
@@ -1,12 +1,82 @@
 showHomeAdmin()
-function showHomeAdmin(){
+
+function showHomeAdmin() {
     $('#body').html(`
- <!-- Categories Start -->
-<div class="container-fluid pt-5">
-    <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span
-            class="bg-secondary pr-3">Admin</span></h2>
-    
+    <div class="container-fluid">
+    <div class="row px-xl-5">
+        <div class="col-lg-8 table-responsive mb-5">
+            <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">ADMIN</span>
+            </h5>
+            <div class="row px-xl-5" id="adminHome">
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="mb-5">
+                <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Notification</span>
+                </h5>
+                <div class="bg-light p-30" id="notification" style="width: 100%; height: 100px;">
+
+                </div>
+            </div>
+            <h5 class=" text-uppercase mb-3"></h5>
+            <div class="bg-light p-30 mb-5" id="total">
+                <button class="btn btn-block btn-primary font-weight-bold my-3 py-3" onclick="findProduct()">Best Products
+                </button>
+                <button class="btn btn-block btn-primary font-weight-bold my-3 py-3" onclick="findProduct()">Best Products
+                </button>
+            </div>
+        </div>
+    </div>
 </div>
-<!-- Categories End -->
     `)
+}
+
+function findProduct(){
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3001/admin/products',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + localStorage.getItem('token')
+        },
+        success: (products) => {
+            let htmlProducts = ''
+            console.log(products)
+            for (let i = 0; i < 8; i++) {
+                htmlProducts += `
+                <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+            <div class="product-item bg-light mb-4">
+                <div class="product-img position-relative overflow-hidden">
+                    <img class="img-fluid w-100" src="public/img/product-2.jpg" alt="">
+                    <div class="product-action">
+                        <a class="btn btn-outline-dark btn-square" onclick="showDetails('${products[i].slug}')"><i class="fa fa-shopping-cart"></i></a>
+                        <a class="btn btn-outline-dark btn-square" onclick="showDetails('${products[i].slug}')"><i class="fa fa-search"></i></a>
+                    </div>
+                </div>
+                <div class="text-center py-4">
+                    <a class="h6 text-decoration-none text-truncate" onclick="showDetails('${products[i].slug}')" >${products[i].name}</a>
+                    <div class="d-flex align-items-center justify-content-center mt-2">
+                        <h5>$${products[i].price} | </h5>
+                        <h6 class="text-muted ml-2">
+                            Sold ${products[i].quantitySold}
+                        </h6>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center mb-1">
+                        <small class="fa fa-star text-primary mr-1"></small>
+                        <small class="fa fa-star text-primary mr-1"></small>
+                        <small class="fa fa-star text-primary mr-1"></small>
+                        <small class="fa fa-star text-primary mr-1"></small>
+                        <small class="fa fa-star text-primary mr-1"></small>
+                        <small>(99)</small>
+                    </div>
+                </div>
+            </div>
+        </div>`
+            }
+            $('#adminHome').html(htmlProducts);
+            let htmlNotice = `<h6>Best 8 products</h6>`
+            $('#notification').html(htmlNotice);
+
+        }
+    })
 }
--- src/public/js/admin/member.js
+++ src/public/js/admin/member.js
@@ -2,7 +2,7 @@ function showMember(role) {
     $('#body').html(`
     <!-- OrderDetails Start -->
 <div class="container-fluid">
-    <div class="row px-xl-5" id="billsDetails">
+    <div class="row px-xl-5" >
         <div class="col-lg-9 table-responsive mb-5">
             <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">List ${role}</span></h5>
             <table class="table table-light table-borderless table-hover text-center mb-0">
@@ -59,7 +59,7 @@ function getListMerchants() {
                 }
                 htmlMerchant += `
         <tr>
-             <td class="align-middle">${item.username}</td>
+             <td class="align-middle" onclick="infoUser('${item.username}')">${item.username}</td>
              <td class="align-middle">${status}</td>
              <td class="align-middle">
                  <a class="btn btn-sm btn-danger" onclick="lockMember('${item.username}', '${item.role}')" ><i ${classIcon}></i>
@@ -92,7 +92,7 @@ function getListUsers() {
                 }
                 htmlUsers += `
         <tr>
-             <td class="align-middle">${item.username}</td>
+             <td class="align-middle" >${item.username}</td>
              <td class="align-middle">${status}</td>
              <td class="align-middle">
                  <a class="btn btn-sm btn-danger" onclick="lockMember('${item.username}', '${item.role}')" ><i ${classIcon}></i>
@@ -123,7 +123,7 @@ function lockMember(username, role) {
             let html = `<h6>${message.message}</h6>`
             $('#notification').html(html)
             let status
-            if (+role === 0){
+            if (+role === 0) {
                 status = 'USERS'
             } else {
                 status = 'MERCHANTS'
@@ -131,6 +131,74 @@ function lockMember(username, role) {
             setTimeout(() => {
                 showMember(status)
             }, 1000)
+        }
+    })
+}
+
+function infoUser(username) {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3001/admin/users/' + username,
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + localStorage.getItem('token')
+        },
+        success: (info) => {
+            console.log(info)
+            let htmlInfo = `
+             <div class="row px-xl-5" i>
+    <div class="col-lg-9 table-responsive mb-5">
+        <div class="container-fluid pb-5">
+            <div class="row px-xl-5">
+                <div class="col-lg-5 mb-30">
+                    <div id="product-carousel" class="carousel slide" data-ride="carousel"  >
+                        <div class="carousel-inner bg-light">
+                            <div class="carousel-item active">
+                                <img class="w-100 h-100" src="public/img/product-2.jpg" alt="Image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-7 h-auto mb-30">
+                    <div class="h-100 bg-light p-30">
+                        <h3>${info.infoShop[0].nameShop}</h3>
+                        <h6 class="font-weight-semi-bold mb-4">Username: ${info.infoUser[0].username}</h6>
+                        <h6 class="font-weight-semi-bold mb-4">Address: ${info.infoShop[0].address}</h6>
+                        <h5 class="mb-4">Infomation: ${info.infoShop[0].information}</h5>
+                        <div class="d-flex pt-2">
+                            <strong class="text-dark mr-2">Share on:</strong>
+                            <div class="d-inline-flex">
+                                <a class="text-dark px-2" href="">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                                <a class="text-dark px-2" href="">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                                <a class="text-dark px-2" href="">
+                                    <i class="fab fa-linkedin-in"></i>
+                                </a>
+                                <a class="text-dark px-2" href="">
+                                    <i class="fab fa-pinterest"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Notification</span>
+        </h5>
+        <div class="bg-light p-30 mb-4" id="notification" style="width: 100%; height: 100px;">
+
+        </div>
+        <button class="btn btn-block btn-primary font-weight-bold my-3 py-3" onclick="showHomeAdmin()">Back
+        </button>
+    </div>
+</div>`
+            $('#body').html(htmlInfo)
         }
     })
 }
